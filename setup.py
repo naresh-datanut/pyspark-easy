0ebf1c57a06b28389095e9110fd2485bb9685975
nareshkumarj90 <36058492+nareshkumarj90@users.noreply.github.com>
2021-03-01 14:00:47 -0600

    changed the files

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'pyspark_easy',         # How you named your package folder (MyLib)
-  packages = ['pyspark_easy'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  name = 'pyspark-easy',         # How you named your package folder (MyLib)
+  packages = ['pyspark-easy'],   # Chose the same as "name"
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Makes pyspark dataframe exploration easy',   # Give a short description about your library
   author = 'Nareshkumar Jayavelu',                   # Type in your name
   author_email = 'nareshkumarj90@gmail.com',      # Type in your E-Mail
-  url = 'https://https://github.com/naresh-datanut/pyspark_easy',   # Provide either the link to your github or to your website
+  url = 'https://https://github.com/naresh-datanut/pyspark-easy',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['pyspark', 'dataframe', 'summary','explore','EDA'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyspark',
           'texttable',
-          'pyspark_easy'
+          'pyspark-easy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
