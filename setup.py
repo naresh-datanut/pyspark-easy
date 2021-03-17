98d0e9963f8a26fb62df189d5419d1de3e945982
nareshkumarj90 <36058492+nareshkumarj90@users.noreply.github.com>
2021-03-16 19:51:39 -0500

    modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyspark_easy',  # How you named your package folder (MyLib)
     packages=['pyspark_easy'],  # Chose the same as "name"
-    version='2.0',  # Start with a small number and increase it with every change you make
+    version='2.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Makes pyspark dataframe exploration easy',  # Give a short description about your library
     author='Nareshkumar Jayavelu',  # Type in your name
